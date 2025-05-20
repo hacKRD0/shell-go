229bdafab01257d9e3bb7776082b1a90de15b2ab
hacKRD0 <dattakeshava@gmail.com>
2025-05-20 16:40:22 -0600

    [any-message]

--- app/utils.go
+++ app/utils.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
 
 func FindInPath(cmd string) (string, bool) {
 	path := os.Getenv("PATH")
+	fmt.Println(path)
 	for _, dir := range strings.Split(path, ";") {
 		filepath := dir + "/" + cmd
 		_, err := os.Stat(filepath);
