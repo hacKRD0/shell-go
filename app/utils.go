bc82ba3a4e7a2eb9c11f56ce96c28114c7154873
hacKRD0 <dattakeshava@gmail.com>
2025-05-20 16:36:46 -0600

    Added command and utils files

--- app/utils.go
+++ app/utils.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 	"strings"
 )
 
 func FindInPath(cmd string) (string, bool) {
 	path := os.Getenv("PATH")
 	for _, dir := range strings.Split(path, ";") {
-		filepath := filepath.Join(dir, cmd)
+		filepath := dir + "/" + cmd
 		_, err := os.Stat(filepath);
 		if err == nil {
 			return filepath, err == nil
 		}
 	}
-	return cmd, false
+	return "", false
 }
